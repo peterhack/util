3d6f0543d991d4a59fddb6e6733d57dabf55c7e7
peterhack <peter.hack@dynatrace.com>
2019-05-20 12:37:48 -0400

    cleanup

--- cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#oc login -u system:admin
-oc delete -n dynatrace oneagent --all
-oc delete -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/master/deploy/openshift.yaml
-oc delete project dynatrace
