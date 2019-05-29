84fd3a9fdfcaca7a04d831b4fbdc4f0fc51a50c3
Peter Hack <peter.hack@dynatrace.com>
2019-05-29 12:44:50 -0400

    updated for OKD config

--- Bastion/make-k8s-admin.sh
+++ Bastion/make-k8s-admin.sh
@@ -1,4 +1,4 @@
 # MAKE BASTION LOCAL CLUSTER ADMIN
 mkdir ~/.kube
-scp master1:~/.kube/config ~/.kube/config
+scp master.openshift.locall:~/.kube/config ~/.kube/config
 echo "made bastion a local cluster admin"
