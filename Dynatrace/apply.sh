30ffb9f2e5ca142aaafd87614c5c83b59c5d837a
EC2 Default User <ec2-user@ip-10-0-1-146.us-west-2.compute.internal>
2019-07-20 00:40:01 +0000

    addressed long urls

--- Dynatrace/apply.sh
+++ Dynatrace/apply.sh
@@ -3,9 +3,8 @@
 # 3 variables are required and can be exported as environment variables:
 # - apiToken    (example: /> export DTAPI="apiToken=1234567890abcdefghijlkmn")
 # - paasToken   (example: /> export DTPAAS="passToken=1234567890abcdefghijklmn")
-# - ENVIRONMENTID (example: /> export TENANTURL="abc12345.dynatrace.live.com"
-#                --for Managed-- /> export TENANTURL="host_url/e/environmentid")
-
+# - ENVIRONMENTID (example: /> export DTENV="abc12345.dynatrace.live.com"
+#                --for Managed-- /> export DTENV="host_url/e/environmentid")
 echo "you require the following details to continue:"
 echo ""
 echo "Dynatrace Environment ID (example abc12345)"
@@ -13,11 +12,19 @@ echo "Dynatrace API Token"
 echo "Dynatrace PaaS Token"
 echo ""
 
-if [ -z "$DTENV" ]; then
-  read -p "Please enter your Dynatrace Environment ID: " DTENV
+read -p "Is this a SaaS tenant? (y/n) " -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  if [ -z "$DTENV" ]; then
+    read -p "Please enter your Dynatrace Environment ID: " DTENV
+    echo ""
+  fi
+else
+  read -p "Please enter the full URL of your tenant:" DTENV
+  FULLURL=true
   echo ""
 fi
-
+  
 if [ -z "$DTAPI" ]; then
   read -p "Please enter your Dynatrace API Token: " DTAPI
   echo ""
@@ -40,7 +47,11 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   if [ $(oc get namespace | grep dynatrace | wc -l) == 0 ];
   then
     oc adm new-project --node-selector="" dynatrace  
-  sed -i 's/ENVIRONMENTID/'"$DTENV"'/' cr.yaml
+  if [ -z "$FULLURL" ]; then
+    sed -i 's/ENVIRONMENTID/'"$DTENV"'/' cr.yaml
+  else
+    sed -i 's~ENVIRONMENTID.live.dynatrace.com~'"$DTENV"'~' cr.yaml
+  fi
   oc create -f openshift.yaml
   oc -n dynatrace create secret generic oneagent --from-literal="apiToken=$DTAPI" --from-literal="paasToken=$DTPAAS"
   oc create -f ./cr.yaml
