81057b89089bcdcdd07fd72a7f4e840799ccb193
Cloud User <centos@ip-172-31-31-250.us-west-2.compute.internal>
2019-07-29 17:49:54 +0000

    fixed typo

--- Bastion/oc-kubectl_setup.sh
+++ Bastion/oc-kubectl_setup.sh
@@ -3,12 +3,12 @@
 # installation of OC and Kubectl configuration
 
 # INSTALL OC & KUBECTL
-wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz -O ~/util/openshift-origin.tar.gz
+wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz -O openshift-origin.tar.gz
 tar -zxf openshift-origin.tar.gz
-  chmod +x ~/util/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc
-  sudo mv ~/util/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /usr/local/bin/oc
-  chmod +x ~/util/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/kubectl
-  sudo mv ~/util/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/kubectl /usr/local/bin/kubectl
-rm -rf ~/util/openshift-origin*
+  chmod +x ./openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc
+  sudo mv ./openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /usr/local/bin/oc
+  chmod +x ./openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/kubectl
+  sudo mv ./openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/kubectl /usr/local/bin/kubectl
+rm -rf ./openshift-origin*
 echo "installed oc & kubectl"
 
